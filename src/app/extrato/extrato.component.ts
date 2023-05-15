0c56f1bffc8648eefb1db2aa06aa1b9e1708ad4b
Frafaelrls <frafaelrls@gmail.com>
2023-05-15 18:17:26 -0300

    adicionando ingecao de TransferenciaService

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,21 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TransferenciaService } from '../services/transferencia.service';
+import { Transferencia } from '../models/transferencia.model';
 
 @Component({
   selector: 'app-extrato',
   templateUrl: './extrato.component.html',
-  styleUrls: ['./extrato.component.scss']
+  styleUrls: ['./extrato.component.scss'],
 })
 export class ExtratoComponent implements OnInit {
+  transferencias: any[] = [];
 
- transferencias: any[] = [];
+  constructor(private service: TransferenciaService) {}
 
- constructor(private service: TransferenciaService) {
-
- }
-
- ngOnInit() {
-     this.transferencias = this.service.transferencias;
- }
-
+  ngOnInit() {
+    this.service.todas().subscribe((transferencias: Transferencia[]) =>{
+      console.table(transferencias);
+      this.transferencias = transferencias;
+    })
+  }
 }
