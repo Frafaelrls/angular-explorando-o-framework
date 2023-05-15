bc1318f9946303a9031aa8135ebe447dcd86f77f
Frafaelrls <frafaelrls@gmail.com>
2023-05-15 15:03:45 -0300

    injetando transferencia service no construtor dos componentes

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TransferenciaService } from '../services/transferencia.service';
 
 @Component({
   selector: 'app-extrato',
   templateUrl: './extrato.component.html',
   styleUrls: ['./extrato.component.scss']
 })
-export class ExtratoComponent {
+export class ExtratoComponent implements OnInit {
 
- @Input() transferencias: any[] = [];
+ transferencias: any[] = [];
+
+ constructor(private service: TransferenciaService) {
+
+ }
+
+ ngOnInit() {
+     this.transferencias = this.service.transferencias;
+ }
 
 }
