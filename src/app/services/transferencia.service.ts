bedcf9637856d96b732e0bd440867d878b743bea
Frafaelrls <frafaelrls@gmail.com>
2023-05-15 18:18:02 -0300

    criando o metodo todas()

--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,12 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Transferencia } from '../models/transferencia.model';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TransferenciaService {
   private listaTransferencia: any[];
+  private url = 'http://localhost:3000/transferencias';
 
-  constructor() {
+  constructor(private httpCliet: HttpClient) {
     this.listaTransferencia = [];
   }
 
@@ -14,12 +18,18 @@ export class TransferenciaService {
     return this.listaTransferencia;
   }
 
+
+  todas(): Observable<Transferencia[]>{
+
+    return this.httpCliet.get<Transferencia[]>(this.url);
+  }
+
   adicionar(transferencia: any) {
     this.hidratar(transferencia);
     this.listaTransferencia.push(transferencia);
   }
 
-  private hidratar(transferencia: any){
+  private hidratar(transferencia: any) {
     transferencia.data = new Date();
   }
 }
